aa708545d796e13fd8da1d7b79248b33fcad8de2
Chiaki Mizuta <chiaki.mizuta@holition.com>
2020-10-09 10:10:52 +0100

    added videocapture

--- imListening.cpp
+++ imListening.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -6,6 +5,21 @@ int main()
 {
     std::cout << "Hello World!\n";
     cv::Mat img = cv::imread("emma.jpg");
-    cv::imshow("emma", img);
-    cv::waitKey(0);
+    cv::Mat frame;
+    cv::VideoCapture cap;
+    cap.open(0);
+    if (!cap.isOpened())
+    {
+        std::cout << "Error opening video stream" << std::endl;
+        return -1;
+    }
+
+    while (1)
+    {
+        cv::Mat frame;
+        cap >> frame;
+        if (frame.empty())
+            break;
+        cv::imshow("frame", frame);
+    }
 }
